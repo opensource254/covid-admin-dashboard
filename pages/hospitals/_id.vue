c970f888baf2059a30b50504ff5424eabe108b55
Stanley Masinde <stanleyloren@gmail.com>
2020-06-16 11:05:20 +0300

    (Hospitals) Ability to delete hospitals by the admin

--- pages/hospitals/_id.vue
+++ pages/hospitals/_id.vue
@@ -4,9 +4,39 @@
       <v-btn large icon to="/hospitals">
         <v-icon>mdi-arrow-left</v-icon>
       </v-btn>
-      <v-btn large icon color="error">
-        <v-icon>mdi-delete-outline</v-icon>
-      </v-btn>
+      <v-dialog v-model="deleteConfirmation" max-width="300px" persistent>
+        <template v-slot:activator="{ on }">
+          <v-btn large icon color="error" v-on="on">
+            <v-icon>mdi-delete-outline</v-icon>
+          </v-btn>
+        </template>
+        <v-card class="text-center" flat outlined shaped>
+          <v-card-text>
+            <h3>Please confirm you want to delete</h3>
+          </v-card-text>
+          <v-card-actions>
+            <v-container>
+              <v-row align-content="center">
+                <v-col cols="6">
+                  <v-btn
+                    block
+                    depressed
+                    color="primary"
+                    large
+                    @click="deleteConfirmation = false"
+                    >Cancel</v-btn
+                  >
+                </v-col>
+                <v-col cols="6">
+                  <v-btn block color="error" outlined @click="deleteHospital"
+                    >Yes Delete</v-btn
+                  >
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-actions></v-card
+        >
+      </v-dialog>
     </v-col>
     <v-col md="8">
       <v-form @submit.prevent="updateHospital">
@@ -77,6 +107,18 @@ export default {
           `/api/v1/hospital/${this.$route.params.id}`,
           this.hospital
         )
+        this.$store.commit('loading', false)
+        this.$store.commit('hideError')
+      } catch (error) {
+        this.$store.commit('loading', false)
+        this.$store.commit('showError', error)
+      }
+    },
+    async deleteHospital() {
+      this.$store.commit('loading', true)
+      try {
+        await this.$axios.delete(`/api/v1/hospital/${this.$route.params.id}`)
+        this.$router.push('/hospitals')
         this.$store.commit('loading', false)
         this.$store.commit('hideError')
       } catch (error) {
