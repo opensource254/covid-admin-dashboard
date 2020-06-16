24a7d88f85db2528e069bb6a0179a03c16be44a8
Stanley Masinde <stanleyloren@gmail.com>
2020-06-16 10:57:30 +0300

    (Tips) The ability of the admin to delete tips

--- pages/tips/_id.vue
+++ pages/tips/_id.vue
@@ -4,9 +4,39 @@
       <v-btn large icon to="/tips">
         <v-icon>mdi-arrow-left</v-icon>
       </v-btn>
-      <v-btn large icon color="error">
-        <v-icon>mdi-delete-outline</v-icon>
-      </v-btn>
+      <v-dialog v-model="deleteConfirmation" max-width="300px" persistent>
+        <template v-slot:activator="{ on }">
+          <v-btn large icon color="error" v-on="on">
+            <v-icon>mdi-delete-outline</v-icon>
+          </v-btn>
+        </template>
+        <v-card class="text-center" flat outlined shaped>
+          <v-card-text>
+            <h3>Please confirm you want to delete</h3>
+          </v-card-text>
+          <v-card-actions>
+            <v-container>
+              <v-row align-content="center">
+                <v-col cols="6">
+                  <v-btn
+                    block
+                    depressed
+                    color="primary"
+                    large
+                    @click="deleteConfirmation = false"
+                    >Cancel</v-btn
+                  >
+                </v-col>
+                <v-col cols="6">
+                  <v-btn block color="error" outlined @click="deleteTip"
+                    >Yes Delete</v-btn
+                  >
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-actions></v-card
+        >
+      </v-dialog>
     </v-col>
     <v-col md="8">
       <v-form @submit.prevent="updateTip">
@@ -49,6 +79,7 @@ export default {
   },
   data() {
     return {
+      deleteConfirmation: false,
       tip: {}
     }
   },
@@ -57,6 +88,21 @@ export default {
       this.$store.commit('loading', true)
       try {
         await this.$axios.put(`/api/v1/tip/${this.$route.params.id}`, this.tip)
+        this.$store.commit('loading', false)
+        this.$store.commit('hideError')
+      } catch (error) {
+        this.$store.commit('loading', false)
+        this.$store.commit('showError', error)
+      }
+    },
+    async deleteTip() {
+      this.$store.commit('loading', true)
+      try {
+        await this.$axios.delete(
+          `/api/v1/tip/${this.$route.params.id}`,
+          this.tip
+        )
+        this.$router.push('/tips')
         this.$store.commit('loading', false)
         this.$store.commit('hideError')
       } catch (error) {
